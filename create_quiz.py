b2c88635ac8f761e259231c9b80f801dca6bc567
Matthew Butner <mfbutner@ucdavis.edu>
2022-04-05 12:36:02 -0700

    Switched to taking the Canvas API Key from the command line.

--- create_quiz.py
+++ create_quiz.py
@@ -1,10 +1,11 @@
 from canvasapi import Canvas
+import sys
 from quiz_creator.participation_quiz import ParticipationQuiz
 from datetime import datetime, timedelta
 from quiz_test import *
 
 if __name__ == "__main__":
-    
+
     # get user input for the url
     # url = getAPIURL()
     # print("Got url: '",url,"'",sep='')
@@ -13,15 +14,15 @@ if __name__ == "__main__":
     # get user input for the api key
     # key = getAPIKEY()
     # print("Got key: '",key,"'",sep='')
-    key = '3438~S5MKJLaQYYFCVtVHFHQnxmSwi1hhoyMx7LfOl9Ih0ecClOUrQJTun5wZ0dzzFxqe'
+    key = sys.argv[1]
 
     # now we have enough information to make our canvas object
     canvas = getCanvas(url, key)
-    print("Got canvas: '",canvas,"'",sep='')
+    print("Got canvas: '", canvas, "'", sep='')
 
     # get user input for the course
     course = getCourse(canvas)
-    print("Got course: '",course,"'",sep='')
+    print("Got course: '", course, "'", sep='')
 
     # Set the new assignment name and the number of interaction questions
     # assignment_name = input("Please name the assignment: ")
@@ -36,7 +37,7 @@ if __name__ == "__main__":
 
     start = datetime.strptime(start, "%m/%d/%Y")
     end = datetime.strptime(end, "%m/%d/%Y")
-    dates_list = [start + timedelta(days=x) for x in range((end-start).days + 1)]
+    dates_list = [start + timedelta(days=x) for x in range((end - start).days + 1)]
 
     # Create the quiz
     quiz = ParticipationQuiz(course, assignment_name, num_interactions, dates_list)
